4b19c37a1621dfd3a91f0e633b8cbf3f6079d88a
albae69 <ahmadalbaihaqi69@gmail.com>
2022-03-21 10:47:03 +0700

    added header state and props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react'
 import Header from './Header'
 
 export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                {
+                    id: 0,
+                    title: 'Naruto Shippuden',
+                    category: 'book',
+                },
+                {
+                    id: 1,
+                    title: 'One Piece',
+                    category: 'book',
+                },
+            ],
+        }
+    }
+
     render() {
         return (
             <div>
-                <Header title='App' />
+                {this.state.data.map((item) => (
+                    <Header key={item.id} title={item.title} />
+                ))}
             </div>
         )
     }
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = ({ title = 'default header' }) => {
-    return <header>ini adalah header {title}</header>
+    return <header>ini adalah buku {title}</header>
 }
 
 export default Header
